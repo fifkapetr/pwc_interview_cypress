5eb5d114569e1d754adc50de1f789af0b52b3c0e
Petr Fifka <petrfifk@gmail.com>
2023-11-20 10:23:26 +0100

    fix loading

--- cypress/e2e/sreality_test.cy.js
+++ cypress/e2e/sreality_test.cy.js
@@ -1,3 +1,5 @@
+import moment from "moment";
+
 describe("SReality Cypress Interview task", () => {
   it("Sreality flat database", () => {
     const date = moment().format("DD.MM.YYYY");
@@ -15,22 +17,23 @@ describe("SReality Cypress Interview task", () => {
     cy.get('div[data-testid="dialog-overlay"]', {
       includeShadowDom: true,
     }).should("not.exist");
-    cy.get("#login").click();
-
     cy.get('a[ng-href="/hledani/byty"]').click();
     cy.get(".suggest-wrap-locality input").type(city);
     cy.get('button[type="submit"]').click();
     cy.get('span[list-select="listSelectConf"] .estate-sort .title').click();
     cy.contains("Nejlevnější").click();
     cy.get("h2").each((element) => {
+      cy.get('img[src="/img/page-loader.gif"]').should("not.be.visible");
       cy.wrap(element).click();
       cy.url();
       cy.get(".location span.location-text").as("street");
       cy.get('span[itemprop="name"] span.name').as("adv_name");
-      cy.get(
-        "body > div:nth-child(3) > div:nth-child(3) > div:nth-child(2) > div:nth-child(3) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(7) > ul:nth-child(1) > li:nth-child(9) > strong:nth-child(2) > span:nth-child(1)"
-      ).as("size");
+      //toto nefunguje ve všech inzerátech - velikost bytu, navrhuju si to pak vytáhnout z názvu
+      //   cy.get(
+      //     "body > div:nth-child(3) > div:nth-child(3) > div:nth-child(2) > div:nth-child(3) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(7) > ul:nth-child(1) > li:nth-child(9) > strong:nth-child(2) > span:nth-child(1)"
+      //   ).as("size");
       cy.get('span[itemprop="price"]').as("price");
+      cy.get(".back a").click();
     });
   });
 });
